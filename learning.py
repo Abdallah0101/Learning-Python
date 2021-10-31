8eb3d95467b67c32df6b90a592f80ec0730e4900
Abdallah Atef <abdallahatefmoh@gmail.com>
2021-10-31 19:09:36 -0300

    Update learning.py

--- learning.py
+++ learning.py
@@ -1,1 +1,22 @@
 print('Hello World')
+
+#المتغيرات
+
+#strings
+
+print('Abda\nlla\nh')
+print('\'abdallah\'')
+
+name = 'Abdallah'
+print(name.lower())
+
+
+print(name.upper().isupper())
+
+print(len(name))
+
+print(name[3])
+
+print(name.index('h'))
+
+print(name.replace('Ab','zo'))
