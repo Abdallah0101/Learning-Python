cbc1a70994d689914303ee1f72ea062c473c803c
Abdallah Atef <abdallahatefmoh@gmail.com>
2021-10-31 19:18:50 -0300

    Update learning.py

--- learning.py
+++ learning.py
@@ -20,3 +20,17 @@ print(name[3])
 print(name.index('h'))
 
 print(name.replace('Ab','zo'))
+
+# % الباقي من قسمة رقم على رقم
+
+print(abs(-5)) #الرقم بدون اشارة
+
+print(pow(2,4)) # اتنين أس أربعة
+
+print(max(5,20,400)) #المقارنة بين مجموعة أرقام واعطاء اكبر رقم وأيضا min
+
+print(round(3.7))
+
+from math import *
+ 
+print(sqrt(16)) #الجذر  التربيعي
